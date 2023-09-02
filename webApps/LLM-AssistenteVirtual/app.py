a3e9fae0c192036b8c5a46e848467c2871ef95d5
Vladimir Costa de Alencar <valencar@gmail.com>
2023-09-02 07:07:48 -0300

    Add files via upload

---
+++ webApps/LLM-AssistenteVirtual/app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+from hugchat import hugchat
+import os
+
+st.set_page_config(page_title="ChatMATE - Chat Multi-purpose AI Technology")
+
+st.title('LLM (Large Language Models) Chat')
+
+# Generate empty lists for bot_response and user_input.
+## bot_response stores AI generated responses
+if 'bot_response' not in st.session_state:
+    st.session_state['bot_response'] = ["I'm ChatMATE, How may I help you?"]
+## user_input stores User's questions
+if 'user_input' not in st.session_state:
+    st.session_state['user_input'] = ['Hi!'] # Hi! why sky is blue ?
+
+# Layout of input/response containers
+input_container = st.container()
+colored_header(label='', description='', color_name='blue-30')
+response_container = st.container()
+
+# User input
+## Function for taking user provided prompt as input
+def get_input():
+    input_text = st.text_input("You: ", "", key="input")
+    return input_text
+## Applying the user input box
+with input_container:
+    user_input = get_input()
+
+# Response output
+## Function for taking user prompt as input followed by producing AI generated responses
+def generate_response(prompt):
+    chatbot = hugchat.ChatBot(cookie_path="cookies.json")
+    response = chatbot.chat(prompt)
+    return response
+
+## Conditional display of AI generated responses as a function of user provided prompts
+with response_container:
+    if user_input:
+        response = generate_response(user_input)
+        st.session_state.user_input.append(user_input)
+        st.session_state.bot_response.append(response)
+        
+    if st.session_state['bot_response']:
+        for i in range(len(st.session_state['bot_response'])):
+            message(st.session_state['user_input'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state['bot_response'][i], key=str(i))
